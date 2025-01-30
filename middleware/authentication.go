955d126d068733fee6859599a2b8231d27768329
e-aub <eaub@proton.me>
2025-01-30 20:09:05 +0100

    rate limiter

--- middleware/authentication.go
+++ middleware/authentication.go
@@ -3,8 +3,10 @@ package middleware
 import (
 	"database/sql"
 	"fmt"
+	"math"
 	"net/http"
 	"os"
+	"sync"
 	"time"
 )
 
@@ -39,4 +41,44 @@ func Middleware(db *sql.DB, next customHandler) http.Handler {
 
 		next(w, r, db, userId)
 	})
+}
+
+type RateLimiter struct {
+	LastTime time.Time
+	Rate     int
+	Capacity int
+	Bucket   int
+}
+
+var UsersLimiters sync.Map
+
+func GetRateLimiter(userId int) *RateLimiter {
+	limiter, ok := UsersLimiters.Load(userId)
+	if !ok {
+		limiter = NewRateLimiter(userId, 5, 10, time.Second)
+		UsersLimiters.Store(userId, limiter)
+	}
+	return limiter.(*RateLimiter)
+}
+func NewRateLimiter(userId, rate, capacity int, limiterTime time.Duration) *RateLimiter {
+	return &RateLimiter{
+		LastTime: time.Now(),
+		Rate:     rate,
+		Capacity: capacity,
+		Bucket:   capacity,
+	}
+}
+
+func (r *RateLimiter) Allow() bool {
+	if time.Since(r.LastTime) >= time.Second {
+		r.LastTime = time.Now()
+		if r.Bucket < r.Capacity {
+			r.Bucket = int(math.Min(float64((r.Bucket + r.Rate)), float64(r.Capacity)))
+		}
+	}
+	if r.Bucket > 0 {
+		r.Bucket--
+		return true
+	}
+	return false
 }
